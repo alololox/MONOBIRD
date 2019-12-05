b688a1c01d7545783fa1612b6a871588ffd6b7cd
Sutcliffe, Alex <Alex.Sutcliffe@arcteryx.com>
2019-12-05 10:05:34 -0800

    add npm component to PDP app

--- apps/pdp/next.config.js
+++ apps/pdp/next.config.js
@@ -1,7 +1,15 @@
-module.exports = {
+const withTM = require("next-transpile-modules");
+
+let config = {
   exportPathMap: function() {
     return {
       "/": { page: "/" }
     };
   }
 };
+
+config = withTM({
+  ...config,
+  transpileModules: ["monobird-test-components"]
+});
+module.exports = config;
