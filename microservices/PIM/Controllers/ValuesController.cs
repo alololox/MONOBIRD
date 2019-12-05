79d1c5d6af93f63afa21a5c819ee6c0027b6a3c1
denomeyb <brian.denomey@arcteryx.com>
2019-12-05 15:31:32 -0800

    just some formatting and cleanup changes to the PDP / PIM app

--- microservices/PIM/Controllers/ValuesController.cs
+++ microservices/PIM/Controllers/ValuesController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Threading.Tasks;
 using cdp_api.data;
 using cdp_api.models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace cdp_api.Controllers
 {
