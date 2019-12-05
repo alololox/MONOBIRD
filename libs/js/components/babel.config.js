277670c9608bbdb943d2c218a68798968ec58659
Sutcliffe, Alex <Alex.Sutcliffe@arcteryx.com>
2019-12-04 19:51:03 -0800

    add testing that works for a monorepo

--- libs/js/components/babel.config.js
+++ libs/js/components/babel.config.js
@@ -1,16 +1,12 @@
 module.exports = {
-    "presets": [
-        [
-            "@babel/preset-env"
-        ],
-        "@babel/preset-react"
-    ],
-    "env": {
-        "test": {
-            "plugins": [
-                "@babel/plugin-transform-modules-commonjs"
-            ],
-        },
-    },
+  presets: [["@babel/preset-env"], "@babel/preset-react"],
+  sourceType: "unambiguous",
+  env: {
+    test: {
+      presets: [["@babel/preset-env"], "@babel/preset-react"],
+      plugins: ["@babel/plugin-transform-modules-commonjs"]
+    }
+  }
 };
-  
+
+console.log("hello");
--- libs/js/components/jest.config.js
+++ libs/js/components/jest.config.js
@@ -1,25 +1,7 @@
-module.export = {
-  roots: ["<rootDir>/src"],
-  transform: {
-    "\\.(js|jsx)?$": "babel-jest"
-  },
-  testMatch: ["<rootDir>/src/**/>(*.)test.{js, jsx}"], // finds test
-  moduleFileExtensions: ["js", "jsx", "json", "node"],
-  testPathIgnorePatterns: ["/node_modules/", "/public/"],
-  setupFilesAfterEnv: [
-    "@testing-library/jest-dom/extend-expect",
-    "@testing-library/react/cleanup-after-each"
-  ],
-  collectCoverageFrom: ["**/*test.{js,jsx}"],
-  coveragePathIgnorePatterns: ["/node_modules/", "package.json", "/coverage/"],
-  collectCoverage: true,
-  coverageReporters: ["text-summary", "lcov", "text", "clover"],
-  coverageThreshold: {
-    global: {
-      branches: 0,
-      functions: 0,
-      lines: 0,
-      statements: 0
-    }
-  }
+const base = require("./jest.config.base.js");
+
+module.exports = {
+  ...base,
+  projects: ["<rootDir>/**/jest.config.js"],
+  coverageDirectory: "<rootDir>/coverage/"
 };
