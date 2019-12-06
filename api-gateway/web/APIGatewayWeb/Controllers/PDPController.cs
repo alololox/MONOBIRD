5d37a0875585ec72498bfe491c5f10d836c93132
anabell <Anabell.Chiu-Carag@arcteryx.com>
2019-12-06 10:07:48 -0800

    Web API Gateway

---
+++ api-gateway/web/APIGatewayWeb/Controllers/PDPController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace APIGatewayWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PDPController : Controller
+    {
+        private readonly List<string> _productNos = new List<string>(new string[] { "1111", "2222", "3333" });
+        private readonly ILogger<PDPController> _logger;
+        public PDPController(ILogger<PDPController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{productNo}")]
+        public string Product(string productNo)
+        {
+            {
+                // authentication, cookies set 
+            }
+            
+            // input validation
+            var foundProduct = _productNos.Find(x => x == productNo);
+            if (foundProduct == null)
+            {                
+                // log any errors
+                _logger.LogWarning($"{productNo} not found.");
+                return "Product not found.";
+            }
+            else
+            {
+                // localization - example: jp, fr
+                // call to services
+                // -- PIM (product info)
+                // -- magento (price)
+                return $"Product service - {productNo}";
+            }
+        }
+    }
+}
