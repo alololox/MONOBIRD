395a0c5728f5563b541811d2c1818dc01f85eec7
denomeyb <brian.denomey@arcteryx.com>
2019-12-06 16:45:32 -0800

    fleshing out the pdp model

--- microservices/Api.Pdp/Helpers/Converters.cs
+++ microservices/Api.Pdp/Helpers/Converters.cs
@@ -5,15 +5,25 @@ namespace Api.Pdp.Helpers
 {
     public static class Converters
     {
-        public static CDP toCDP(this recordsRecord pimRecord,string language = "en", string region = "ca")
+        public static PdpModel toPdpModel(this recordsRecord pimRecord, string language = "en", string region = "ca")
         {
+            PdpModel pdpModel = new PdpModel();
+
+            //details
+            var x = pimRecord.fields.Where(x => x.name == "id").Select(x => x.values).First();
+            pdpModel.Details.ProductNo = x.Length > 0 ? int.Parse(x[0].Value) : 0;
+            //pdpModel.Details.ProductNo = int.Parse(pimRecord.fields.Where(x => x.name == "id").Select(x => x.values).First()[0].Value.ToString());
+            pdpModel.Details.LargeImageUrl = "https://images-dynamic-arcteryx.imgix.net/F19/450x625/Axe-Keeper-Black.png?auto=format"; //hardcoded for now
+
+            //translated fields
             var names = pimRecord.fields.Where(x => x.name == "name").Select(x => x.values).First();
+            var descriptions = pimRecord.fields.Where(x => x.name == "shortdescription").Select(x => x.values).First();
+            pdpModel.Name = names.FirstOrDefault(x => x.language == language)?.Value;
+            pdpModel.Details.TranslatedDescr = descriptions.FirstOrDefault(x => x.language == language)?.Value;
 
-            CDP cDP = new CDP();
-            cDP.Name = names.FirstOrDefault(x => x.language == "EN")?.Value;
-         //   cDP.Original = pimRecord; 
-            return cDP; 
 
+            //   cDP.Original = pimRecord; 
+            return pdpModel; 
         }
     }
 }
---
+++ microservices/Api.Pdp/Models/PdpModel.cs
@@ -0,0 +1,33 @@
+﻿namespace Api.Pdp.Models
+{
+    public class PdpModel
+    {
+        public PdpModel()
+        {
+            Details = new ProductDetailModel();
+        }
+
+        //   public recordsRecord Original {get;set;}
+        
+        // primitives
+        public string Name { get; set; }
+
+        //to fully represent a product, eventually we will have something that looks more like this (copied from arc.com)
+        public ProductDetailModel Details { get; set; }
+        //public List<ProductSkuModel> SkuDetails { get; set; }
+        //public List<ProductClassificationModel> Classifications { get; set; }
+        //public Dictionary<string, ColorDataModel> ColorDict { get; set; }
+        //public List<KeyWordDataModel> KeyWords { get; set; }
+        //public ProductMerchandisingModel Merchandising { get; set; }
+        //public List<ProductSizeSchemeModel> SizeSchemes { get; set; }
+        //public object Colors { get; set; }
+        //public List<ProductExcludedColors> ExcludedColors { get; set; }
+
+        //as well as definitions for all those subclasses as needed
+    }
+
+
+
+    
+
+}
