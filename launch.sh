7e7e418ebad4b3eacef7b71e410778358140268b
arcmodal <morgan.dalman@arcteryx.com>
2019-12-13 16:34:21 -0800

    Cleanup for demo

--- launch.sh
+++ launch.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 cd apps/cdp
-ASSET_PREFIX=http://localhost:3001 npm run build && PORT=3001 npm run start &
+npm install
+ASSET_PREFIX=http://localhost:3001  PORT=3001 npm run dev &
 
-cd ../pdp
-ASSET_PREFIX=http://localhost:3002 npm run build && PORT=3002 npm run start &
+cd apps/pdp
+npm install
+ASSET_PREFIX=http://localhost:3002 PORT=3002 npm run dev &
 
 cd ../..
 cp gateway/.env .
 sed -i.bak "s/CDP_PAGES_ORIGIN=.*/CDP_PAGES_ORIGIN=host.docker.internal:3001/g" .env
-sed -i.bak "s/PDP_ORIGIN=.*/PDP_ORIGIN=host.docker.internal:3001/g" .env
+sed -i.bak "s/PDP_ORIGIN=.*/PDP_ORIGIN=host.docker.internal:3002/g" .env
 rm .env.bak
-ENV_FILE=../.env docker-compose -f gateway/docker-compose.yml up
+#ENV_FILE=../.env docker-compose -f gateway/docker-compose.yml up
 
 
-trap "exit" INT TERM
-trap "kill 0" EXIT
+#trap "exit" INT TERM
+#trap "kill 0" EXIT
