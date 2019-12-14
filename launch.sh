f6f3010459b2f9ff26bab4d781bf282652f03540
Malcolm Mcdell <malcolm.mcdell@amersports.com>
2019-12-13 17:14:10 -0800

    updated launch

--- launch.sh
+++ launch.sh
@@ -4,15 +4,16 @@ cd apps/cdp
 npm install
 ASSET_PREFIX=http://localhost:3001  PORT=3001 npm run dev &
 
-cd apps/pdp
+cd ../pdp
 npm install
 ASSET_PREFIX=http://localhost:3002 PORT=3002 npm run dev &
 
-cd ../..
-cp gateway/.env .
-sed -i.bak "s/CDP_PAGES_ORIGIN=.*/CDP_PAGES_ORIGIN=host.docker.internal:3001/g" .env
-sed -i.bak "s/PDP_ORIGIN=.*/PDP_ORIGIN=host.docker.internal:3002/g" .env
-rm .env.bak
+#moved to compose for now. 
+#cd ../..
+#cp gateway/.env .
+#sed -i.bak "s/CDP_PAGES_ORIGIN=.*/CDP_PAGES_ORIGIN=host.docker.internal:3001/g" .env
+#sed -i.bak "s/PDP_ORIGIN=.*/PDP_ORIGIN=host.docker.internal:3002/g" .env
+#rm .env.bak
 #ENV_FILE=../.env docker-compose -f gateway/docker-compose.yml up
 
 
