1ee41308bf897574fa0c35a06ebcb93e339d6417
Ron Santos <ron.santos@arcteryx.com>
2019-12-16 15:46:25 -0800

    Update ProductTile to display Hero image from JSON data instead of placeholder

--- libs/js/components/core/ProductTile.stories.js
+++ libs/js/components/core/ProductTile.stories.js
@@ -4,12 +4,16 @@ import { storiesOf } from "@storybook/react";
 import { ProductTile } from "./index.js";
 
 storiesOf("ProductTile", module).add("text test", () => {
-  const product = { 
-    name: 'Alpha SV Jacket Men\'s',
-    url: '/shop/mens/alpha-sv-jacket' 
+  const product = {
+    name: "Alpha SV Jacket Men's",
+    url: "/shop/mens/alpha-sv-jacket",
+    heroImage:
+      "https://images.arcteryx.com/F19/350w/Alpha-SV-Jacket-Labyrinth.jpg"
   };
 
-  return <>
-    <ProductTile product={ product } />
-  </>
+  return (
+    <>
+      <ProductTile product={product} />
+    </>
+  );
 });
